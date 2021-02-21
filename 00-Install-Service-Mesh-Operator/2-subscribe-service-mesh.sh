67c0fc929c61ddd5ac87a2f510080c4f5ef3917a
Phil Huang <phil.huang@redhat.com>
2021-02-21 10:04:52 +0800

    Fixed #2
    Signed-off-by: Phil Huang <phil.huang@redhat.com>

--- 00-Install-Service-Mesh-Operator/2-subscribe-service-mesh.sh
+++ 00-Install-Service-Mesh-Operator/2-subscribe-service-mesh.sh
@@ -4,6 +4,7 @@
 # Red Hat OpenShift Service Mesh 1.1.2
 # https://docs.openshift.com/container-platform/4.4/service_mesh/service_mesh_install/installing-ossm.html#ossm-operator-install-kiali_installing-ossm
 
+oc apply -f create-project-openshift-operators-redhat.yaml
 oc apply -f elasticsearch-operator-subscription.yaml
 oc apply -f jaeger-product-operator-subscription.yaml
 oc apply -f kiali-ossm-operator-subscription.yaml
